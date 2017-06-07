f9dd46c432b8facac3d4298354a5ecf155a78de7
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2017-06-07 20:38:20 +0000

    Add auth info to start_stop script.

--- policy/tasks/start_stop_vms.py
+++ policy/tasks/start_stop_vms.py
@@ -2,6 +2,7 @@
 from ks_auth import sess
 from ks_auth import ks
 from novaclient import client
+from novaclient import exceptions
 # RDTIBCC-983
 # Satisfies Requirements:
 #  - start a vm
@@ -15,6 +16,47 @@ username = 'standard_user'
 search_opts = {
     'all_tenants': True
 }
+
+def access_info_vars(session):
+    """Expects keystoneclient.session.Session"""
+    to_return = [
+        'trust_id',
+        'trust_scoped',
+        'trustee_user_id',
+        'trustor_user_id',
+        'user_domain_name',
+        'username',
+    ]
+    ai = ks.auth.client.session.auth.get_access(sess)
+    ret = map(lambda k: (k, getattr(ai, k)), to_return)
+    return dict(ret)
+
+
+#s.auth.auth_methods[0].get_auth_data(s,s.auth,{})
+#('password', {'user': {'domain': {'name': 'default'}, 'password': 'VZ3fVYlsjDqpTkwCY8M6', 'name': 'admin'}})
+def initial_auth_info(session, auth_filter=lambda x: x[0] == 'password'):
+    ret = []
+    auth = session.auth
+    auth_data = map(lambda x: x.get_auth_data(session, auth, {}),
+                     auth.auth_methods)
+    filtered = filter(auth_filter, auth_data)
+    for authtype, params in filtered:
+        try:
+            del params['user']['password']
+        except KeyError:
+            pass
+        ret.append((authtype, params))
+    return ret
+
+# Print some info
+print('Initial Auth Info:')
+for authtype, params in initial_auth_info(ks.auth.client.session):
+    print(' %s' % authtype)
+    print('  %s' % params)
+
+print('Access Info:')
+for k, v in access_info_vars(sess).iteritems():
+    print('* {}: {}'.format(k, v))
 
 # list all servers
 # BUG(kmidzi): all_tenants=True appears busted
@@ -30,8 +72,18 @@ user_servers = nova.servers.list(search_opts=search_opts)
 print(user_servers)
 
 target_servers = filter(lambda x: x.user_id == user.id, user_servers)
+print('Targeting:')
 print(target_servers)
 
 for server in target_servers:
-    server.stop()
+    try:
+        print('* Stopping %s' % server.name)
+        server.stop()
+    except exceptions.Conflict, e:
+        target_msg = r' it is in vm_state stopped'
+        if e.message.find(target_msg) != -1:
+            print('** skipping stop action - already stopped.')
+        else:
+            raise
+    print('* Starting %s' % server.name)
     server.start()
