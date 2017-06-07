151ca99e7223ca6ce87ef1c2dbe5d6f1cbd16aa1
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2017-06-07 18:51:16 +0000

    Add trust creation script
    Need to circumvent the additional API calls from openstackclient cli that may
    fail policy checks.

---
+++ policy/create_trust.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from ks_auth import ks
+import sys
+import config
+
+trustee = config.trustee
+if __name__ == '__main__':
+    try:
+        trustee_id = sys.argv[1]
+    except IndexError:
+        sys.exit('Supply a trustee id')
+
+    projects = ks.auth.projects()
+    names = map(lambda x: x.name, projects)
+    print('Available projects: %s' % names)
+
+    # Make sure to match authenticated user
+    trustor = ks.auth.client.get_user_id()
+    impersonate = True
+
+    for project in projects:
+        project_id = project.id
+        args = {
+            'trustee_user': trustee_id,
+            'trustor_user': trustor,
+            'role_names': ['Member'],
+            'project': project_id,
+            'impersonation': impersonate,
+        }
+        res = ks.trusts.create(**args)
+        print(res)
