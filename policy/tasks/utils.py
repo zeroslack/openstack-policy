ad63378ef51979ebdd2db9e72103406ef429ce2a
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2017-06-16 13:32:21 +0000

    Offload to small util module.

---
+++ policy/tasks/utils.py
@@ -0,0 +1,30 @@
+def render_obj(attrs=[]):
+    def inner(objname):
+        return dict(filter(lambda x: x[0] in attrs,
+                           objname.__dict__.iteritems()))
+    return inner
+
+
+render_vol_attachment = render_obj(
+    attrs = [
+        'device',
+        'id',
+        'serverId',
+        'volumeId',
+    ]
+)
+
+render_volume = render_obj(
+    attrs = [
+        'description',
+        'id',
+        'imageRef',
+        'instance',
+        'metadata',
+        'name',
+        'project_id',
+        'size',
+        'user_id',
+        'volume_type',
+    ]
+)
