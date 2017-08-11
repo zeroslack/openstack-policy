bd014451fef742ae5bfd1919f2368144749e5b82
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2017-08-11 21:34:18 +0000

    Re-write list_floating_ips.py to use keystoneauth1 sessions + url
    floating_ips submodule was deprecated at some point. In order to list
    floating ips in the same manner from Nova, the call is made by issuing
    a request to relevant URL instead, which should still function, given
    the API server still supports it. See:
    
    https://developer.openstack.org/api-ref/compute/#floating-ips-os-floating-ips-deprecated

--- policy/tasks/list_floating_ips.py
+++ policy/tasks/list_floating_ips.py
@@ -1,10 +1,40 @@
 #!/usr/bin/env python
-from ks_auth import sess
+from ka_auth import sess
 from novaclient import client
+from pbr.version import SemanticVersion
+from pbr.version import VersionInfo
+import keystoneclient.exceptions
+import os
+import sys
+try:
+    import simplejson as json
+except ImportError:
+    import json
 
-# floating_ips module deprecated as of python-novaclient >= 8.0.0
+
+MAX_SEMVER = SemanticVersion(major=7, minor=1, patch=2)
+semver = VersionInfo('python-novaclient').semantic_version()
+
+
 version = 2
 nova = client.Client(version, session=sess)
 print('Nova API: %s' % nova.api_version)
-ip_list = nova.floating_ips.list()
-print(ip_list)
+
+# floating_ips module deprecated as of python-novaclient >= 8.0.0
+if semver <= MAX_SEMVER:
+    ip_list = nova.floating_ips.list()
+    print(ip_list)
+else:
+    endpoint_filter = {
+        'service_type': 'compute',
+        'interface': 'public',
+        # TODO(kamidzi): may break??
+        'region_name': os.environ.get('OS_REGION'),
+    }
+    try:
+        #https://developer.openstack.org/api-ref/compute/#floating-ips-os-floating-ips-deprecated
+        resp = sess.get('/os-floating-ips',
+                        endpoint_filter=endpoint_filter)
+        print(json.dumps(resp.json(), indent=2))
+    except keystoneclient.exceptions.NotFound:
+        pass
