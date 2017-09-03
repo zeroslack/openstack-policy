f2a0f4736741a22b57f0261c4e1796a4f10d617a
zeroslack <kundai.andrew.midzi@gmail.com>
2017-09-03 04:57:44 +0000

    Switch to new domain-qualified paradigm.

--- policy/create_test_instances.sh
+++ policy/create_test_instances.sh
@@ -7,14 +7,14 @@ username='standard_user'
 
 dump_openrc(){
 	cat <<EoF
-export OS_PROJECT_NAME=$project_name
-export OS_USERNAME=$username
-export OS_PASSWORD=$password
+export OS_PROJECT_NAME='$project_name'
+export OS_USERNAME='$username'
+export OS_PASSWORD='$password'
 export OS_AUTH_URL=${OS_AUTH_URL:-https://openstack.bcpc.example.com:5000/v3/}
-export OS_REGION_NAME=${OS_REGION_NAME:-Test-Laptop-Vagrant}
+export OS_REGION_NAME='${OS_REGION_NAME:-Test-Laptop-Vagrant}'
 export OS_NO_CACHE=$OS_NO_CACHE
-export OS_PROJECT_DOMAIN_NAME=$OS_PROJECT_DOMAIN_NAME
-export OS_USER_DOMAIN_NAME=$OS_USER_DOMAIN_NAME
+export OS_PROJECT_DOMAIN_NAME='$OS_PROJECT_DOMAIN_NAME'
+export OS_USER_DOMAIN_NAME='$OS_USER_DOMAIN_NAME'
 export OS_IDENTITY_API_VERSION=$OS_IDENTITY_API_VERSION
 export OS_IMAGE_API_VERSION=$OS_IMAGE_API_VERSION
 export OS_COMPUTE_API_VERSION=$OS_COMPUTE_API_VERSION
@@ -22,13 +22,18 @@ export OS_VOLUME_API_VERSION=$OS_VOLUME_API_VERSION
 EoF
 }
 # create the project
-openstack project create --or-show $project_name
+openstack project create --or-show --domain "$OS_PROJECT_DOMAIN_NAME" "$project_name"
 # create the user
-openstack user create --or-show --project $project_name --password $password \
-	$username
+openstack user create --or-show --domain "$OS_USER_DOMAIN_NAME" \
+  --project-domain "$OS_PROJECT_DOMAIN_NAME" \
+  --project "$project_name" --password "$password" "$username"
+openstack user set --password "$password" $username
 # assign the roles
-for rolename in ${roles[@]}; do
-	openstack role add --project $project_name --user $username $rolename
+for rolename in "${roles[@]}"; do
+  openstack role add --project "$project_name" \
+    --project-domain "$OS_PROJECT_DOMAIN_NAME" \
+    --user-domain "$OS_USER_DOMAIN_NAME" \
+    --user "$username" "$rolename" || true
 done
 
 rc_file=user-rc
@@ -39,7 +44,7 @@ dump_openrc > "$rc_file"
 flavor='generic1.small'
 image='Cirros 0.3.4 x86_64'
 instances=( test_server1 )
-for i in ${instances[@]}; do
-	openstack server show $i || \
-		openstack server create --flavor $flavor --image "$image" --wait $i
+for i in "${instances[@]}"; do
+	openstack server show "$i" || \
+		openstack server create --flavor "$flavor" --image "$image" --wait "$i"
 done
--- policy/create_test_user.sh
+++ policy/create_test_user.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -eu
 . /root/adminrc
 
 load_config(){
@@ -18,14 +19,14 @@ load_config
 
 dump_openrc(){
 	cat <<EoF
-export OS_PROJECT_NAME=$project_name
-export OS_USERNAME=$testuser
-export OS_PASSWORD=$password
+export OS_PROJECT_NAME='$project_name'
+export OS_USERNAME='$testuser'
+export OS_PASSWORD='$password'
 export OS_AUTH_URL=$OS_AUTH_URL
-export OS_REGION_NAME=$OS_REGION_NAME
+export OS_REGION_NAME='$OS_REGION_NAME'
 export OS_NO_CACHE=$OS_NO_CACHE
-export OS_PROJECT_DOMAIN_NAME=$OS_PROJECT_DOMAIN_NAME
-export OS_USER_DOMAIN_NAME=$OS_USER_DOMAIN_NAME
+export OS_PROJECT_DOMAIN_NAME='$OS_PROJECT_DOMAIN_NAME'
+export OS_USER_DOMAIN_NAME='$OS_USER_DOMAIN_NAME'
 export OS_IDENTITY_API_VERSION=$OS_IDENTITY_API_VERSION
 export OS_IMAGE_API_VERSION=$OS_IMAGE_API_VERSION
 export OS_COMPUTE_API_VERSION=$OS_COMPUTE_API_VERSION
@@ -36,13 +37,17 @@ EoF
 exec 3>&1
 exec 1>&2
 # create the project
-openstack project create --or-show $project_name
+openstack project create --or-show --domain "$OS_PROJECT_DOMAIN_NAME" "$project_name"
 # create the user
-openstack user create --or-show --project $project_name --password $password \
-	$testuser
+openstack user create --or-show --domain "$OS_USER_DOMAIN_NAME" \
+  --project-domain "$OS_PROJECT_DOMAIN_NAME" \
+  --project "$project_name" --password "$password" "$testuser"
+openstack user set --password "$password" $testuser
 # assign the roles
-for rolename in ${roles[@]}; do
-	openstack role add --project $project_name --user $testuser $rolename
+for rolename in "${roles[@]}"; do
+	openstack role add --project "$project_name" \
+    --project-domain "$OS_PROJECT_DOMAIN_NAME" \
+    --user-domain "$OS_USER_DOMAIN_NAME" \
+    --user "$testuser" "$rolename" || true
 done
-dump_openrc >&3
-exec 3>&-
+dump_openrc >&3 exec 3>&-
